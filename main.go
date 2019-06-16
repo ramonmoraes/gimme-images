eb4beae3fa4a897850d3e1141d29d0577a0acb8a
Ramon Moraes <ramon.moraes94@gmail.com>
2019-06-16 02:14:04 -0300

    main getting images from url

--- main.go
+++ main.go
@@ -3,18 +3,27 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	gimme "ramonmoraes/gimme-images/gimme"
 )
 
 func main() {
+	urls := gimme.CrawlURL("https://ocapacitor.com/john-wick-3-parabellum-filme-tem-novas-cenas-divulgadas/")
+	errs := gimme.DownloadURLS(urls)
+	if len(errs) != 1 {
+		log.Fatal(errs)
+	}
+}
+
+func sample() {
 	bytes, err := ioutil.ReadFile("data/fixture.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	doc := getDocument(bytes)
-	srcs := getImagesSRC(doc)
+	doc := gimme.GetDocument(bytes)
+	srcs := gimme.GetImagesSRC(doc)
 	for _, src := range srcs {
-		img := Image{URL: src}
+		img := gimme.Image{URL: src}
 		img.Download()
 		img.Save()
 	}
