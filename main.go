599ab91a7de88c8b4e90ce65c678abed65e8dd9d
Ramon Moraes <ramon.moraes94@gmail.com>
2019-06-16 01:56:03 -0300

    move img logic to Image model

--- main.go
+++ main.go
@@ -1,50 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
 )
 
 func main() {
-	bytes, err := ioutil.ReadFile("./fixture.html")
+	bytes, err := ioutil.ReadFile("data/fixture.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	doc := getDocument(bytes)
-	imgs := getImagesSRC(doc)
-	fmt.Println(imgs)
-}
-
-func example() {
-	url := "https://i2.wp.com/ocapacitor.com/wp-content/uploads/2016/08/0000jgpfymtl.jpg?fit=655%2C370&ssl=1&resize=350%2C200"
-	filepath := ".dart/img.jpg"
-	err := downloadImage(filepath, url)
-	if err != nil {
-		log.Fatal(err)
+	srcs := getImagesSRC(doc)
+	for _, src := range srcs {
+		img := Image{URL: src}
+		img.Download()
+		img.Save()
 	}
-}
-
-func downloadImage(filepath string, url string) error {
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
 }
