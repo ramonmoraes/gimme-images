03fd13f1b3651880df9a0dce1bf584c9c715dbe9
Ramon Moraes <ramon.moraes94@gmail.com>
2019-06-21 16:58:42 -0300

    extract save srcs to new func

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
+
 	gimme "ramonmoraes/gimme-images/gimme"
 )
 
 func main() {
-	urls := gimme.CrawlURL("https://ocapacitor.com/john-wick-3-parabellum-filme-tem-novas-cenas-divulgadas/")
-	errs := gimme.DownloadURLS(urls)
-	if len(errs) != 1 {
-		log.Fatal(errs)
+	crawler := gimme.Crawler{Domain: "https://ocapacitor.com"}
+	urls := crawler.CrawlURL("https://ocapacitor.com/john-wick-3-parabellum-filme-tem-novas-cenas-divulgadas/")
+	saveSrcs(urls)
+}
+
+func saveSrcs(srcs []string) {
+	errors := []error{}
+	for _, src := range srcs {
+		img := gimme.Image{URL: src}
+		d_err := img.Download()
+		if d_err != nil {
+			errors = append(errors, d_err)
+		}
+		s_err := img.Save()
+		if s_err != nil {
+			errors = append(errors, s_err)
+		}
+	}
+	logErrors(errors)
+}
+
+func logErrors(errors []error) {
+	for _, e := range errors {
+		fmt.Println(e)
 	}
 }
 
@@ -22,9 +44,5 @@ func sample() {
 
 	doc := gimme.GetDocument(bytes)
 	srcs := gimme.GetImagesSRC(doc)
-	for _, src := range srcs {
-		img := gimme.Image{URL: src}
-		img.Download()
-		img.Save()
-	}
+	saveSrcs(srcs)
 }
